18d82b3f978985b2cc346ce99dbf7dc157f89b13
Frank Wang <frank7300228@gmail.com>
2022-05-20 16:48:11 +0800

    Finish 113

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -26,7 +26,9 @@ class CsvFileReader {
                 row[1],
                 row[2],
                 parseInt(row[3]),
-                parseInt(row[4])
+                parseInt(row[4]),
+                row[5],
+                row[6]
             ];
         });
     }
